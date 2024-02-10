4824c7539b1df585c07659674c44b00747ada2ca
Burak Duvakli <70605238+SoofiBD@users.noreply.github.com>
2024-02-10 14:42:47 +0300

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -20,16 +20,28 @@ class Result {
 
     public static void staircase(int n) {
     // Write your code here
-
+        
+        //We need to print "n" rows because the height of the staricase in "n". This is done a using a for loop that itarets 1 --> n.
         for (int row = 1; row <=n; row++){
+          /*
+            Before the # symbols in each row, we need to print spaces. 
+            The number of spaces decreases as we move down the staircase. 
+            For the first row, we print n-1 spaces, for the second row n-2, and so on, until we print 0 spaces on the last row. 
+            This is achieved with another for-loop inside our row loop.
+            */
             for (int space = 1; space <= n - row; space++){
                 System.out.print(" ");
             }
-
+           /*After the spaces, we print # symbols. 
+           The number of # symbols starts at 1 in the first row and increases by 1 for each subsequent row until it reaches n in the bottom row. 
+           This is also done using a for-loop.
+           */
             for (int hash = 1; hash <= row; hash++){
                 System.out.print("#");
             }
-
+            /*After printing the spaces and # symbols for a row, we need to move to the next line to start printing the next row. 
+            This is done by printing a newline character.
+            */
             System.out.println();
         }
     
@@ -49,3 +61,8 @@ public class Solution {
         bufferedReader.close();
     }
 }
+
+/*
+The goal is to print a staircase of size n using # symbols and spaces.
+The staircase is right-aligned, meaning that the base and height are equal to n, and it looks something like this for n=4
+*/
